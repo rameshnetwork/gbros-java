8a58983f4546c350713fa1a937008b25c30f82e4
91916 <ramesh.bym@gmail.com>
2024-06-01 14:52:25 +0530

    deployemnt file

--- src/main/java/com/gbros/controller/AppController.java
+++ src/main/java/com/gbros/controller/AppController.java
@@ -3,11 +3,18 @@ package com.gbros.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class AppController {
 
-	@GetMapping("/test")
-	public String gelloWorld() {
-		return "Hello World!";
-	}
+    @GetMapping("/test")
+    public Map<String, String> gelloWorld() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "java");
+        map.put("2", "javk8sa");
+
+        return map;
+    }
 }
